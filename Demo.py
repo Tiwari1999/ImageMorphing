f0f91e29a31a744f7045c7474de6fde4c412cdeb
fuenwang <tdk356ubuntu@gmail.com>
2016-12-28 21:18:08 +0800

    gg

--- Demo.py
+++ Demo.py
@@ -3,6 +3,10 @@
 import cv2
 import numpy as np
 
+def GetTransform(theta):
+    cos = np.cos
+    sin = np.sin
+    return np.array([[cos(theta), sin(theta)], [-sin(theta), cos(theta)]], dtype=np.float32)
 class Demo:
     def __init__(self, img1, img2): # source, destination
         self.img1 = cv2.imread(img1)
@@ -10,7 +14,7 @@ class Demo:
         self.img1_ori = self.img1.copy()
         self.img2_ori = self.img2.copy()
         
-        # init x y, dest x y
+        # [[init x y]], [[dest x y]]
         self.line_1 = [[], []]
         self.line_2 = [[], []]
         self.warpline = None
@@ -75,6 +79,7 @@ class Demo:
                 #print self.line_2
                 cv2.destroyAllWindows()
         self.LineInterp(frames)
+        self.Morphing(frames)
     def LineInterp(self, frames):
         line1 = self.line_1
         line2 = self.line_2
@@ -114,18 +119,68 @@ class Demo:
         #[np.zeros([total_line, 2]), np.zeros([total_line, 2])]
         self.warpline = [[np.zeros([total_line, 2]), np.zeros([total_line, 2])] for x in range(frames)]
         for frame in range(frames):
-            self.warpline[frame][0][:, 0] = interp_buf_start_y[:, frame]
-            self.warpline[frame][0][:, 1] = interp_buf_start_x[:, frame]
-            self.warpline[frame][1][:, 0] = interp_buf_end_y[:, frame]
-            self.warpline[frame][1][:, 1] = interp_buf_end_x[:, frame]
+            self.warpline[frame][0][:, 1] = interp_buf_start_y[:, frame]
+            self.warpline[frame][0][:, 0] = interp_buf_start_x[:, frame]
+            self.warpline[frame][1][:, 1] = interp_buf_end_y[:, frame]
+            self.warpline[frame][1][:, 0] = interp_buf_end_x[:, frame]
         #print self.warpline[0][0]
         #print self.warpline[-1][0]
         #print self.warpline
-    #def Morphing(self, frames):
-                
 
+    def Morphing(self, frames):
+        [height, width, _] = self.img1.shape
+        total_line = self.line_1[0].shape[0]
+        h = np.arange(height, dtype=np.float32)
+        w = np.arange(width, dtype=np.float32)
+        [x, y] = np.meshgrid(w, h)
+        #print x
+        #print y
+        pixel_map = np.zeros([height, width, 2], dtype=np.float32)
+        pixel_map[:,:, 0] = x
+        pixel_map[:,:, 1] = y
+        pixel_map = pixel_map.reshape([height*width, 2]).T
+        #print pixel_map
+        
+        for frame in range(1, frames):
+            warpline_dest = self.warpline[frame]
+            warpline_source = self.warpline[frame-1] 
+            
+            P_map_dest = warpline_dest[0]
+            Q_map_dest = warpline_dest[1]
+
+            P_map_source = warpline_source[0]
+            Q_map_source = warpline_source[1]
+
+            vec_map_dest = Q_map_dest - P_map_dest
+            vec_map_source = Q_map_source - P_map_source 
+
+            len_map_dest = np.linalg.norm(vec_map_dest, axis=1)
+            len_map_source = np.linalg.norm(vec_map_source, axis=1)
+
+            img = np.zeros([height, width], dtype=np.float32)
+            
+            for line in range(total_line):
+                cos_theta = np.dot(vec_map_source[line], vec_map_dest[line])/(len_map_dest[line]*len_map_source[line])
+                theta = np.arccos(cos_theta)
+                T = GetTransform(theta)
+                pt_mapping = np.dot(T, pixel_map)
+                print pt_mapping
+            '''     
+            for x in range(width):
+                for y in range(height):
+                    Weight = np.zeros([1, total_line], dtype=np.float32)
+                    for line in range(total_line):
+                        PX = pixel_map[y, x] - P_map_dest[line]
+                        PQ = vec_map_dest[line]
+                        u = np.abs(np.dot(PX, PQ) / (len_map_dest[line]))
+                        v = np.sqrt(np.linalg.norm(PX)**2 - u**2)
+                        u /= len_map_dest[line]
+                        [new_x, new_y] = P_map_source[line] + u*(vec_map_source[line]) \
+                        + v
+                        #exit()
+            '''
 test = Demo('image.jpg', 'depth.png')
-test.Run('GG', 'TT', 10)
+test.Run('GG', 'TT', 5)
 print 'origin data'
 #print test.line_1
 #print test.line_2
