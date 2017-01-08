0263920fce2af4b1c51f4910e88d07e65d7d0dac
fuenwang <tdk356ubuntu@gmail.com>
2017-01-09 03:07:36 +0800

    gg

--- Demo.py
+++ Demo.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
+import os
 import cv2
 import numpy as np
-import image2gif as GIF
 from numpy import matlib
 from PIL import Image
 
@@ -18,12 +18,12 @@ def GetTransform(theta):
 
 def Perpendicular(vec):
     tmp = np.zeros(vec.shape, dtype=np.float32)
-    tmp[:, 0] = vec[:, 1]
-    tmp[:, 1] = -vec[:, 0]
+    tmp[:, 0] = -vec[:, 1]
+    tmp[:, 1] = vec[:, 0]
     s = np.sqrt(np.sum(tmp**2, axis=1).reshape([-1, 1]))
     s = matlib.repmat(s, 1, 2)
     #print s
-   # tmp /= s
+    #tmp /= s
     return tmp
 
 class Demo:
@@ -94,31 +94,20 @@ class Demo:
         self.line_2[1] = np.array(self.line_2[1])
 
     def _Morphing(self):
+        #os.system('mkdir')
+        os.system('rm out*.jpg')
         height = self.img1_ori.shape[0]
         width = self.img1_ori.shape[1]
 
-        line_1 = self.line_1
-        line_2 = self.line_2
+        line_2 = self.line_1
+        line_1 = self.line_2
 
-        start_1 = line_1[0]
-        start_2 = line_2[0]
-        end_1 = line_1[1]
-        end_2 = line_2[1]
-        line_count = start_1.shape[0]
-        # 2 war to 1 
-        vec_1 = end_1 - start_1 # source
-        vec_2 = end_2 - start_2 # dest
-        len_1 = np.sqrt(np.sum((start_1 - end_1)**2, axis=1))
-        len_2 = np.sqrt(np.sum((start_2 - end_2)**2, axis=1))
-        theta = np.arccos(np.sum(vec_1 * vec_2, axis=1) / (len_1 * len_2))
-        delta = -np.sign(vec_2[:, 0] * vec_1[:, 1] - vec_2[:, 1] * vec_1[:, 0]) 
-        theta *= delta
-        Transform = GetTransform(theta)
-        #print vec_1
-        #print Perpendicular(vec_1) * vec_1
-        #print Transform
-        #exit()
-        # (y, x)
+        start_1_map = line_1[0]
+        start_2_map = line_2[0]
+        end_1_map = line_1[1]
+        end_2_map = line_2[1]
+        line_count = start_1_map.shape[0]
+
         loc_map = np.zeros([height, width, 2])
         loc_map[:,:,1] = matlib.repmat(np.arange(height).reshape([-1,1]), 1, width)
         loc_map[:,:,0] = matlib.repmat(np.arange(width), height, 1)
@@ -126,21 +115,25 @@ class Demo:
         #print loc_map[:5]
         #print loc_map[-5:]
         #exit()
+        img_lst = []
         repmat = matlib.repmat
         [a,b,p] = self._GetABP()
-        for frame_index in range(self._frame_count - 2):
+        for frame_index in range(self._frame_count-1):
+            start_1 = start_1_map.copy()
+            end_1 = end_1_map.copy()
+            start_2 = ((start_2_map-start_1_map).astype(np.float32)/(self._frame_count-1))*(frame_index+1) + start_1_map
+            end_2 = ((end_2_map-end_1_map).astype(np.float32)/(self._frame_count-1))*(frame_index+1) + end_1_map
+
             # ratio for ori and dest image
-            ratio = float(frame_index+1) / (self._frame_count - 1)
-            X_pron_lst = []
-            W_lst = []
+            ratio = float(frame_index) / (self._frame_count + 1)
+            X_pron_lst_1 = []
+            W_lst_1 = []
+            X_pron_lst_2 = []
+            W_lst_2 = []
             for line_index in range(line_count):
                 X = loc_map.copy()
                 Q = repmat(end_2[line_index, :], width*height, 1)
                 P = repmat(start_2[line_index, :], width*height, 1)
-                #print X.shape
-                #print Q.shape
-                #print P.shape
-                #exit()
                 X_sub_P = X - P
                 Q_sub_P = Q - P
                 Q_P_len_square = np.sum(Q_sub_P**2, axis=1)
@@ -153,10 +146,6 @@ class Demo:
                 
                 Q_sub_P_pron = Q_pron - P_pron
                 Q_sub_P_pron_len = np.sqrt(np.sum(Q_sub_P_pron**2, axis=1))
-                #print Q_sub_P_pron_len[0]
-                #print min(V)
-                #print max(V)
-                #exit()
                 X_pron = P_pron + repmat(U.reshape([-1,1]), 1, 2) * Q_sub_P_pron \
                         + repmat(V.reshape([-1,1]), 1, 2) * Perpendicular(Q_sub_P_pron) \
                         / repmat(Q_sub_P_pron_len.reshape([-1,1]), 1, 2)
@@ -169,51 +158,96 @@ class Demo:
                 i2 = U < 0
                 i3 = U > 1
                 W[i1, :] = np.abs(V[i1]).reshape([-1, 1])
-                W[i3, :] = X_P_pron_len[i3].reshape([-1, 1])
-                W[i2, :] = X_Q_pron_len[i2].reshape([-1, 1])
+                W[i2, :] = X_P_pron_len[i2].reshape([-1, 1])
+                W[i3, :] = X_Q_pron_len[i3].reshape([-1, 1])
                 W = ((Q_sub_P_pron_len.reshape([-1,1]))**p / (a + W))**b
                 
-                #print np.max(X_pron[:,1])
-                #print np.min(X_pron[:,1])
-                #print np.max(W)
-                #print np.min(W)
-                #exit()
-                '''
-                X_pron = np.round(X_pron)
-                X_pron[X_pron[:, 0] >= width, 0] = width - 1
-                X_pron[X_pron[:, 0] < 0, 0] = 0
-                X_pron[X_pron[:, 1] >= height, 1] = height - 1
-                X_pron[X_pron[:, 1] < 0, 1] = 0
-                '''
-                X_pron_lst.append(X_pron)
-                W_lst.append(W)
-                #print X_pron.shape
-                #print W.shape
-                #print np.max(W)
-                #print np.min(W)
-            X_sum = np.zeros([width*height, 2], dtype=np.float32)
-            W_sum = np.zeros([width*height, 1], dtype=np.float32)
-            for i, w in enumerate(W_lst):
-                X_sum += X_pron_lst[i] * repmat(w, 1, 2)
-                W_sum += w
-            X_final = X_sum / repmat(W_sum, 1, 2)
-            X_final = np.round(X_final).astype(np.int32)
-            X_final[X_final[:, 0] >= width, 0] = width - 1
-            X_final[X_final[:, 0] < 0, 0] = 0
-            X_final[X_final[:, 1] >= height, 1] = height - 1
-            X_final[X_final[:, 1] < 0, 1] = 0
+                X_pron_lst_1.append(X_pron)
+                W_lst_1.append(W)
+
+            start_1 = start_2_map.copy()
+            end_1 = end_2_map.copy()
+            start_2 = (((start_2_map-start_1_map).astype(np.float32)/(self._frame_count-1))*(frame_index+1) + start_1_map)
+            end_2 = (((end_2_map-end_1_map).astype(np.float32)/(self._frame_count-1))*(frame_index+1) + end_1_map)
+
+            for line_index in range(line_count):
+                X = loc_map.copy()
+                Q = repmat(end_2[line_index, :], width*height, 1)
+                P = repmat(start_2[line_index, :], width*height, 1)
+                X_sub_P = X - P
+                Q_sub_P = Q - P
+                Q_P_len_square = np.sum(Q_sub_P**2, axis=1)
+
+                U = np.sum(X_sub_P * Q_sub_P, axis=1) / Q_P_len_square
+                V = np.sum(X_sub_P * Perpendicular(Q_sub_P), axis=1) / np.sqrt(Q_P_len_square)
                 
+                Q_pron = repmat(end_1[line_index, :], width*height, 1)
+                P_pron = repmat(start_1[line_index, :], width*height, 1)
+                
+                Q_sub_P_pron = Q_pron - P_pron
+                Q_sub_P_pron_len = np.sqrt(np.sum(Q_sub_P_pron**2, axis=1))
+                X_pron = P_pron + repmat(U.reshape([-1,1]), 1, 2) * Q_sub_P_pron \
+                        + repmat(V.reshape([-1,1]), 1, 2) * Perpendicular(Q_sub_P_pron) \
+                        / repmat(Q_sub_P_pron_len.reshape([-1,1]), 1, 2)
+
+                X_P_pron_len = np.sqrt(np.sum((X_pron - P_pron)**2, axis=1))
+                X_Q_pron_len = np.sqrt(np.sum((X_pron - Q_pron)**2, axis=1))
+
+                W = np.zeros([width*height, 1], np.float32)
+                i1 = U <= 1
+                i2 = U < 0
+                i3 = U > 1
+                W[i1, :] = np.abs(V[i1]).reshape([-1, 1])
+                W[i2, :] = X_P_pron_len[i2].reshape([-1, 1])
+                W[i3, :] = X_Q_pron_len[i3].reshape([-1, 1])
+                W = ((Q_sub_P_pron_len.reshape([-1,1]))**p / (a + W))**b
+                
+                X_pron_lst_2.append(X_pron)
+                W_lst_2.append(W)
+
+            X_sum_1 = np.zeros([width*height, 2], dtype=np.float32)
+            W_sum_1 = np.zeros([width*height, 1], dtype=np.float32)
+            for i, w in enumerate(W_lst_1):
+                X_sum_1 += X_pron_lst_1[i] * repmat(w, 1, 2)
+                W_sum_1 += w
+            X_sum_2 = np.zeros([width*height, 2], dtype=np.float32)
+            W_sum_2 = np.zeros([width*height, 1], dtype=np.float32)
+            for i, w in enumerate(W_lst_2):
+                X_sum_2 += X_pron_lst_2[i] * repmat(w, 1, 2)
+                W_sum_2 += w
+
+            X_final_1 = X_sum_1 / repmat(W_sum_1, 1, 2)
+            X_final_1 = np.round(X_final_1).astype(np.int32)
+            X_final_1[X_final_1[:, 0] >= width, 0] = width - 1
+            X_final_1[X_final_1[:, 0] < 0, 0] = 0
+            X_final_1[X_final_1[:, 1] >= height, 1] = height - 1
+            X_final_1[X_final_1[:, 1] < 0, 1] = 0
+
+            X_final_2 = X_sum_2 / repmat(W_sum_2, 1, 2)
+            X_final_2 = np.round(X_final_2).astype(np.int32)
+            X_final_2[X_final_2[:, 0] >= width, 0] = width - 1
+            X_final_2[X_final_2[:, 0] < 0, 0] = 0
+            X_final_2[X_final_2[:, 1] >= height, 1] = height - 1
+            X_final_2[X_final_2[:, 1] < 0, 1] = 0
+               
             source_img = self.img1_ori.astype(np.float32)
             dest_img = self.img2_ori.astype(np.float32)
             #img = np.zeros([height*width, 3], dtype=np.float32)
             #print source_img[X_final].shape
-            img = (1-ratio) * source_img[X_final[:,1], X_final[:, 0], :].reshape([height*width,-1]) + (ratio) \
-                  * dest_img.reshape([height*width,-1])
+            img = (1-ratio) * source_img[X_final_1[:,1], X_final_1[:, 0], :].reshape([height*width,-1]) + (ratio) \
+                    * dest_img[X_final_2[:,1], X_final_2[:, 0], :].reshape([height*width,-1])
             img = img.reshape([height, width, -1]).astype(np.uint8)
             name = 'out_%d.jpg'%frame_index
             cv2.imwrite(name, img)
+            img_lst.append(name)
             #print np.max(W_sum)
             #print np.min(W_sum)
+        img_lst.append(self._img2_name)
+        s = ''
+        for one in img_lst:
+            s += '%s '%one
+        command = 'convert -delay 30 -loop 0 %s result.gif'%s
+        os.system(command)
 
     def Run(self, window1, window2, frames):
         self._frame_count = frames
@@ -234,8 +268,8 @@ class Demo:
                 cv2.destroyAllWindows()
         self._Morphing()
 
-test = Demo('1.jpg', '2.jpg', 1, 2, 0)
-test.Run('GG', 'TT', 10)
+test = Demo('7.jpg', '8.jpg', 1, 2, 0)
+test.Run('GG', 'TT', 20)
 print 'origin data'
 # print test.line_1
 # print test.line_2
